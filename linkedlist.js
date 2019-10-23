78309db2016822af6edc88392752d630aebe370c
Dora Gil <github@doragil.me>
2019-10-23 01:08:52 +0000

    remove specific data value from ll

--- linkedlist.js
+++ linkedlist.js
@@ -29,12 +29,40 @@ class LinkedList {
             this.size++;
         }
     }
+
+    remove(data) {
+        if (!this.head) {
+            console.log('There is nothing in this list. Get your -ish together. Thank you :)')
+            return;
+        }
+        if (this.head.data === data) {
+            this.head = this.head.next;
+            this.size--;
+            return;
+        }
+
+        let current = this.head;
+        let previous;
+        while (current && current.data !== data) {
+            previous = current;
+            current = current.next;
+        }
+        if (current === null) {
+            console.log('Data not Found');
+            return;
+        }
+        previous.next = current.next;
+        this.size--;
+    }
 }
 
 const myLinkedList = new LinkedList();
-
+myLinkedList.remove(50);
 myLinkedList.insertLast(50);
+myLinkedList.remove(50);
 myLinkedList.insertFirst(30);
 myLinkedList.insertLast(70);
+myLinkedList.remove(70);
+myLinkedList.remove(20);
 
-console.log(myLinkedList.head)
+console.log(myLinkedList)
